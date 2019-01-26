e37635563cdd047984b5fc3846fa2623a6039713
Lee Palacios <vitualizz@gmail.com>
2019-01-26 15:39:15 -0500

    root

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  root 'home#index'
   get 'disconnect' => 'users#disconnect'
   resources :socials
   resources :posts
-  root 'home#index'
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   resource :users, only: :show 
   get 'home/contact'
