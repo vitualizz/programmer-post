d5ba343e54c656e1d4b49155dff950e275bcfa3e
Lee Palacios <vitualizz@gmail.com>
2019-01-26 17:48:46 -0500

    initialize

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,7 +14,7 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 #Rails.application.config.assets.precompile += %w( home.css devise.css )
 
 Rails.application.config.assets.precompile << Proc.new { |path|
-  if path =~ /\.(css|js)\z/
+  if path =~ /\.(css|js|sass)\z/
     full_path = Rails.application.assets.resolve(path).pathmap
     app_assets_path = Rails.root.join('app', 'assets').to_path
     if full_path.starts_with? app_assets_path
